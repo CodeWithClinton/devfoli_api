353a9057c0e506f0f9a2d7bb09dfdee36654a4e6
CodeWithClinton <clintonmatics@gmail.com>
2024-11-11 12:16:10 +0100

    new commit

--- myblogapi/settings.py
+++ myblogapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-md&n@26=i3g(a8x9l=)b(anqey!gw41n&w@@ofob)qu!3*h5mh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["devfoli-api.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
 MEDIA_URL = 'img/'
 MEDIA_ROOT = BASE_DIR/'media'
 
